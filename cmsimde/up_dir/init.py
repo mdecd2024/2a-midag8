07438d861ef15dcb6e722270043330d1eccd82c8
KEHUEISIN <41023108@gm.nfu.edu.tw>
2024-03-08 16:08:56 +0800

    add 41023110 子模組

--- cmsimde/up_dir/init.py
+++ cmsimde/up_dir/init.py
@@ -12,7 +12,7 @@ class Init(object):
     uwsgi = False
     site_title = "cmsimde_site"
     ip = "127.0.0.1"
-    dynamic_port = 9443
+    dynamic_port = 9445
     static_port = 8443
     def __init__(self):
         # hope to create downloads and images directories　
